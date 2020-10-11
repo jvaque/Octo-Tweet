224f4dc50ba109ee6b213a3cb2db99f01ccfa87f
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2020-10-11 03:23:31 +0100

    Created Default Console App
    Created the default console app to serve as the basis of the rest of the
    program

---
+++ Quarterback/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quarterback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
