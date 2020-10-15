4d426431fb9d5639b6f46d3ceb919bd7a57d5fea
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2020-10-16 00:45:03 +0100

    Make Retrieving The Full History Pararell
    Made the multiple calls when retrieving the full history run parallel on
    top of being async for improved performance

--- Quarterback/StartService.cs
+++ Quarterback/StartService.cs
@@ -28,33 +28,63 @@ namespace Quarterback
 
             ElectricityModel electricityUse = await _octopusElectricityHelper.GetConsumption(electricityMPAN, electricitySerialNumber);
 
-            double electricityPages = Math.Ceiling(electricityUse.Count / 100d);
-
-            List<ConsumptionDetail> allHistoryElectricityUse = new List<ConsumptionDetail>();
-            for (int page = 1; page < (electricityPages+1); page++)
-            {
-                allHistoryElectricityUse.AddRange((await _octopusElectricityHelper.GetConsumptionPage(page, electricityMPAN, electricitySerialNumber)).Results);
-            }
-
+            List<ConsumptionDetail> allHistoryElectricityUse = await AllHistoryElectricity(electricityUse.Count);
 
             string gasMPAN = _config.GetValue<string>("OctopusApi:Gas:mpan");
             string gasSerialNumber = _config.GetValue<string>("OctopusApi:Gas:serial_number");
 
             GasModel gasUse = await _octopusGasHelper.GetConsumption(gasMPAN, gasSerialNumber);
 
-            double gasPages = Math.Ceiling(gasUse.Count / 100d);
+            List<ConsumptionDetail> allHistoryGasUse = await AllHistoryGas(gasUse.Count);
 
-            List<ConsumptionDetail> allHistoryGasUse = new List<ConsumptionDetail>();
-            for (int page = 1; page < (gasPages + 1); page++)
-            {
-                allHistoryGasUse.AddRange((await _octopusGasHelper.GetConsumptionPage(page, gasMPAN, gasSerialNumber)).Results);
-            }
         }
 
-        // Todo make calls to the electricity and gas endpoint in their distinct async calls for better performance
-        //private async Task<List<ConsumptionDetail>> Electricity()
-        //{
+        private async Task<List<ConsumptionDetail>> AllHistoryElectricity(double useCount)
+        {
+            string electricityMPAN = _config.GetValue<string>("OctopusApi:Electricity:mpan");
+            string electricitySerialNumber = _config.GetValue<string>("OctopusApi:Electricity:serial_number");
 
-        //}
+            double electricityPages = Math.Ceiling(useCount / 100d);
+
+            List<ConsumptionDetail> electricityHistory = new List<ConsumptionDetail>();
+            List<Task<ElectricityModel>> tasks = new List<Task<ElectricityModel>>();
+
+            for (int page = 1; page < (electricityPages + 1); page++)
+            {
+                tasks.Add(_octopusElectricityHelper.GetConsumptionPage(page, electricityMPAN, electricitySerialNumber));
+            }
+
+            var results = await Task.WhenAll(tasks);
+            foreach (var item in results)
+            {
+                electricityHistory.AddRange(item.Results);
+            }
+
+            return electricityHistory;
+        }
+
+        private async Task<List<ConsumptionDetail>> AllHistoryGas(double useCount)
+        {
+            string gasMPAN = _config.GetValue<string>("OctopusApi:Gas:mpan");
+            string gasSerialNumber = _config.GetValue<string>("OctopusApi:Gas:serial_number");
+
+            double gasPages = Math.Ceiling(useCount / 100d);
+
+            List<ConsumptionDetail> gasHistory = new List<ConsumptionDetail>();
+            List<Task<GasModel>> task = new List<Task<GasModel>>();
+
+            for (int page = 1; page < (gasPages + 1); page++)
+            {
+                task.Add(_octopusGasHelper.GetConsumptionPage(page, gasMPAN, gasSerialNumber));
+            }
+
+            var results = await Task.WhenAll(task);
+            foreach (var item in results)
+            {
+                gasHistory.AddRange(item.Results);
+            }
+
+            return gasHistory;
+        }
     }
 }
