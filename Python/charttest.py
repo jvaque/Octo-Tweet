d602f02da3fefdee87974cb9a7c6d35af3545ddb
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2020-11-01 18:17:50 +0000

    Create First Charts With Matplotlib
    Create a chart with matplotlib and save it to an image, also watched
    https://www.youtube.com/watch?v=UO98lJQ3QGI to help understand certain
    things of the library

--- Python/charttest.py
+++ Python/charttest.py
@@ -1,9 +1,10 @@
-import matplotlib
+# import matplotlib
+from matplotlib import pyplot, dates
 from datetime import datetime
 import csv
 
 listOfUse = []
-with open('data.csv') as csv_file:
+with open('data2electricBoogaloo.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     
     for row in csv_reader:
@@ -24,7 +25,27 @@ with open('data.csv') as csv_file:
 
         listOfUse.append([row[0], row[1], datetimeFrom, datetimeTo])
 
-print(listOfUse)
+energyList = []
+counterList = []
+
+for element in listOfUse:
+    energyList.append(float(element[1]))
+    counterList.append(dates.date2num(element[2]))
 
 # datetimeFromTimestamp = datetime.fromisoformat("2020-09-23 01:00:00+00:00:00")
 # print(datetimeFromTimestamp)
+
+pyplot.bar(counterList, energyList, color='#444444')
+pyplot.plot(counterList, energyList, color='#0000ff', marker='.')
+
+pyplot.xlabel('Time of day (h)')
+pyplot.ylabel('Energy Consumption (kWh)')
+pyplot.title('24th Sept 2020')
+
+# pyplot.legend()
+
+pyplot.grid(True)
+
+pyplot.savefig('plot.png')
+
+pyplot.show()
