599c2b54cb0444e2e1c7fdc2a05a1121432af9e8
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2020-11-05 02:04:45 +0000

    Fix Displaying Datetimes
    After fully understanding how the datetimes are stored changed the way
    the graph is created so it reflects the correct values. In the database
    datetime values are stored with the timezone information for that
    datetime value, with those two values the corresponding utc datetime can
    be derived.

--- Python/charttest.py
+++ Python/charttest.py
@@ -9,20 +9,24 @@ with open('data2electricBoogaloo.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     
     for row in csv_reader:
-        # Test to see if the datetime is in a timezone before UTC
-        #  (not really applicable for my use case but thought it would be nice to
-        #  cover all edge cases)
-        if row[3][0] != "-":
-            extraChar = "+"
-        else:
-            extraChar = ""
-        datetimeFrom = datetime.fromisoformat(row[2]+extraChar+row[3])
+        # Datetime is stored as current timezone datetime + offset to UTC
+        #  from that timezone
 
-        if row[5][0] != "-":
-            extraChar = "+"
-        else:
-            extraChar = ""
-        datetimeTo = datetime.fromisoformat(row[4]+extraChar+row[5])
+        ## No need for taing into acount the offset because the datetime is in local time
+        ##  offset only tells what timezone the datetime was taken
+        # if row[3][0] != "-":
+        #     extraChar = "+"
+        # else:
+        #     extraChar = ""
+        # datetimeFrom = datetime.fromisoformat(row[2]+extraChar+row[3])
+        datetimeFrom = datetime.fromisoformat(row[2])
+
+        # if row[5][0] != "-":
+        #     extraChar = "+"
+        # else:
+        #     extraChar = ""
+        # datetimeTo = datetime.fromisoformat(row[4]+extraChar+row[5])
+        datetimeTo = datetime.fromisoformat(row[4])
 
         listOfUse.append([row[0], row[1], datetimeFrom, datetimeTo])
 
@@ -31,18 +35,23 @@ timeList = []
 
 for element in listOfUse:
     energyList.append(float(element[1]))
-    timeList.append(dates.date2num(element[2]))
+    timeList.append(element[2])
 
 # datetimeFromTimestamp = datetime.fromisoformat("2020-09-23 01:00:00+00:00:00")
 # print(datetimeFromTimestamp)
 
 x_indexes = np.arange(len(timeList))
 
-
-pyplot.bar(x_indexes, energyList, color='#babae8')
+pyplot.plot(timeList, energyList, color='#babae8')
 # pyplot.plot(x_indexes, energyList, color='#0000ff', marker='.')
 
-pyplot.xticks(ticks=x_indexes, labels=timeList)
+# pyplot.xticks(ticks=x_indexes, labels=timeList)
+
+ax = pyplot.axes()
+
+xfmt = dates.DateFormatter('%H:%M')
+# ax.set_xtics(x_indexes)
+ax.xaxis.set_major_formatter(xfmt)
 
 pyplot.title('24th Sept 2020')
 pyplot.xlabel('Time of day (h)')
