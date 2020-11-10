0a580e2fa1409a697f896d73f5a541dadee4f712
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2020-11-10 01:45:52 +0000

    Test Tweetpy Following Tutorials
    Retrieve the latest tweets from the timeline using the tweetpy library
    and also use retrieve the api secret keys from appsettings.json

--- Python/tweettest.py
+++ Python/tweettest.py
@@ -1,1 +1,20 @@
 import tweepy
+import json
+
+# Retrieve values from config file
+with open('appsettings.json', 'r') as f:
+    config = json.load(f)
+
+consumer_key = config['TwitterApi']['consumer_key']
+consumer_secret = config['TwitterApi']['consumer_secret']
+access_token = config['TwitterApi']['access_token']
+access_token_secret = config['TwitterApi']['token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
