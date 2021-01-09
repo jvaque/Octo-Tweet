8232e1b246e911ae9c44980f6bcd7645e9f571cf
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-01-09 02:45:33 +0000

    Stored Procedure To Retrieve Records From Range
    Added two stored procedures to retrieve all the records in the database
    between the specified time periods.

--- MySQL/octo_tweet/dbo/Permissions/prOcto_tweet_sp.sql
+++ MySQL/octo_tweet/dbo/Permissions/prOcto_tweet_sp.sql
@@ -1,8 +1,10 @@
 #--------------------------------------------------------------------------------------------------
 GRANT EXECUTE ON PROCEDURE octo_tweet.spElectricity_GetLatestSavedRecord TO 'octo_tweet_sp'@'localhost';
+GRANT EXECUTE ON PROCEDURE octo_tweet.spElectricity_GetRecordsFromRange TO 'octo_tweet_sp'@'localhost';
 GRANT EXECUTE ON PROCEDURE octo_tweet.spElectricity_Insert TO 'octo_tweet_sp'@'localhost';
 #--------------------------------------------------------------------------------------------------
 GRANT EXECUTE ON PROCEDURE octo_tweet.spGas_GetLatestSavedRecord TO 'octo_tweet_sp'@'localhost';
+GRANT EXECUTE ON PROCEDURE octo_tweet.spGas_GetRecordsFromRange TO 'octo_tweet_sp'@'localhost';
 GRANT EXECUTE ON PROCEDURE octo_tweet.spGas_Insert TO 'octo_tweet_sp'@'localhost';
 #--------------------------------------------------------------------------------------------------
 
---
+++ MySQL/octo_tweet/dbo/Stored_Procedures/Gas/spGas_GetRecordsFromRange.sql
@@ -0,0 +1,32 @@
+DROP PROCEDURE IF EXISTS octo_tweet.spGas_GetRecordsFromRange;
+
+DELIMITER //
+
+CREATE PROCEDURE octo_tweet.spGas_GetRecordsFromRange(
+    IN Interval_datetime_from datetime,
+    IN Interval_datetime_to datetime
+)
+BEGIN
+
+    SELECT
+        gas_id,
+        gas_consumption,
+        gas_interval_start_datetime,
+        gas_interval_start_offset,
+        gas_interval_end_datetime,
+        gas_interval_end_offset
+    FROM
+        Gas
+    WHERE
+        gas_interval_start_datetime >= Interval_datetime_from
+    AND
+        gas_interval_end_datetime <= Interval_datetime_to
+    ORDER BY
+        gas_interval_start_datetime ASC
+    ;
+
+END
+
+//
+
+DELIMITER ;
