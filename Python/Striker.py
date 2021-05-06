8662b35e5108718541d030d115171ebcdd6f2fa1
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-05-06 01:39:49 +0100

    Add Subparsers For Different Functions
    Splitting the parsing into two subparsers to split functionality, also
    propperly implemented the main function

--- Python/Striker.py
+++ Python/Striker.py
@@ -3,46 +3,56 @@ import argparse
 import datetime
 
 
-def parseArguments(args_in):
+def parseArguments():
     # Create the parser
     parser = argparse.ArgumentParser(description='Generate and Tweet charts.',
                                      allow_abbrev=False)
 
-    group = parser.add_mutually_exclusive_group()
+    subparser = parser.add_subparsers()
+
+    manual_parser = subparser.add_parser('charts')
+    data_parser = subparser.add_parser('generate')
+
+    data_group = data_parser.add_mutually_exclusive_group()
 
     # Add the arguments
     parser.version = '1.0.0'
 
-    parser.add_argument('datetime_from',
-                        action='store',
-                        type=datetime.datetime.fromisoformat,
-                        help='datetime from for chart in ISO format')
+    manual_parser.add_argument(
+        'datetime_from',
+        action='store',
+        type=datetime.datetime.fromisoformat,
+        help='datetime from for chart in ISO format')
 
-    parser.add_argument('datetime_to',
-                        action='store',
-                        type=datetime.datetime.fromisoformat,
-                        help='datetime to for chart in ISO format')
+    manual_parser.add_argument(
+        'datetime_to',
+        action='store',
+        type=datetime.datetime.fromisoformat,
+        help='datetime to for chart in ISO format')
 
-    parser.add_argument('-c',
-                        '--charts',
-                        action='store',
-                        nargs='+',
-                        choices=['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly'],
-                        help='select the type of charts to generate between the given dates')
+    manual_parser.add_argument(
+        'charts',
+        action='store',
+        nargs='+',
+        choices=['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly'],
+        help='select the type of charts to generate between the given dates')
 
-    group.add_argument('-t',
-                        '--tweet',
-                        action='store_true',
-                        help='tweet the generated charts')
+    data_group.add_argument(
+        '-t',
+        '--tweet',
+        action='store_true',
+        help='tweet the generated charts')
 
-    group.add_argument('-l',
-                        '--lazy',
-                        action='store_true',
-                        help='skip generating charts, only update database')
+    data_group.add_argument(
+        '-l',
+        '--lazy',
+        action='store_true',
+        help='skip generating charts, only update database')
 
-    parser.add_argument('-v',
-                        '--version',
-                        action='version')
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version')
 
     # parser.add_argument()
     # parser.add_argument_group()
@@ -51,6 +61,8 @@ def parseArguments(args_in):
     args = parser.parse_args()
     
     print(vars(args)) # Just here for placing a breakpoint
+
+    return args
 
 # def main(*args):
 #     # Take in arguments to select what type of graphs to make and tweet
@@ -68,12 +80,12 @@ def parseArguments(args_in):
 
 #     # Log when the program was run and if it was successful
 
+def main():
+    args = parseArguments()
+
+    if(args.charts != None):
+        for chart in args.charts:
+            print(chart)
+
 if __name__ == "__main__":
-    # print("So far the arguments I want to be able to take in are as follow:")
-    # print("--day --week --month --year -d -w -m -y")
-    # "args": ["--day", "--week", "--month", "--year", "-d", "-w", "-m", "-y"]
-    # print("And if I think of more I will add them in the future")
-    # print("You have started the app")
-    # print()
-    parseArguments(sys.argv)
-    # main(*sys.argv[1:])
+    main()
