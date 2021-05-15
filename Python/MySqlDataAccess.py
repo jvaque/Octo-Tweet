49917c29a9348ad08bb464cae4f89da2bc104864
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-05-15 16:12:19 +0100

    Remove Catching Exceptions In MySqlDataAccess
    Removed the catching of exceptions in the MySqlDataAccess as they were
    just blanket catches.

--- Python/MySqlDataAccess.py
+++ Python/MySqlDataAccess.py
@@ -2,14 +2,14 @@ class MySqlDataAccess:
     def __init__(self, config):
         self._config = config
 
-    def callStoredProcedure(self, storedProcedureName, args, connectionStringName):
+    def loadData(self, storedProcedureName, args, connectionStringName):
         try:
             cnx = mysql.connector.connect(
                 user=self._config[connectionStringName]['user_name'],
                 password=self._config[connectionStringName]['password'],
                 host=self._config[connectionStringName]['host'],
                 database=self._config[connectionStringName]['database_name'])
-            
+
             cursor = cnx.cursor()
 
             resultArgs = cursor.callproc(storedProcedureName, args)
@@ -17,18 +17,10 @@ class MySqlDataAccess:
             for element in cursor.stored_results():
                 listOfResults = element.fetchall()
 
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
         finally:
             cursor.close()
             cnx.close()
-        
+
         return listOfResults
 
     def saveData(self, storedProcedureName, args, connectionStringName):
@@ -38,19 +30,11 @@ class MySqlDataAccess:
                 password=self._config[connectionStringName]['password'],
                 host=self._config[connectionStringName]['host'],
                 database=self._config[connectionStringName]['database_name'])
-            
+
             cursor = cnx.cursor()
 
             cursor.callproc(storedProcedureName, args)
             cnx.commit()
-
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
 
         finally:
             cursor.close()
